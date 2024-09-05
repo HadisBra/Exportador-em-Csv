883da4458337f9b51933d4e7cc276262778d10e1
Guilherme Alexandre Pereira Lima <113989944+HadisBra@users.noreply.github.com>
2024-09-05 11:05:31 -0300

    Add files via upload

---
+++ exportador-simple-factory/src/main/java/Principal.java
@@ -0,0 +1,32 @@
+import com.manoelcampos.exportador.ExportadorListaProduto;
+import com.manoelcampos.exportador.Produto;
+
+import java.util.List;
+
+/**
+ * Executa a aplicação Exportador Simple Factory.
+ * @author Manoel Campos da Silva Filho
+ */
+public class Principal {
+    public static void main(String[] args) {
+        final var listaProdutos = List.of(
+            new Produto("TV", "LG", "132-A", 120),
+            new Produto("Notebook", "Asus", "New age", 341),
+            new Produto("Smartphone", "Samsung", "Galaxy S10", 214)
+        );
+
+        final var exportadorPadrao = ExportadorListaProduto.newInstance("html");
+        System.out.println("Lista de Produtos em HTML\n");
+        System.out.println(exportadorPadrao.exportar(listaProdutos));
+
+        final var exportadorMarkdown = ExportadorListaProduto.newInstance("md");
+        System.out.println("Lista de Produtos em Markdown\n");
+        System.out.println(exportadorMarkdown.exportar(listaProdutos));
+
+        final var exportadorCSV = ExportadorListaProduto.newInstance("csv");
+        System.out.println("Lista de Produtos em CSV\n");
+        System.out.println(exportadorCSV.exportar(listaProdutos));
+    }
+
+}
+
